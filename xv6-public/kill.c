7efcb3a04fa0c8dba26593188ca7199299f5513d
HectorRAD <a01636166@itesm.mx>
2020-09-02 01:18:33 +0000

    practica 2

--- xv6-public/kill.c
+++ xv6-public/kill.c
@@ -11,7 +11,13 @@ main(int argc, char **argv)
     printf(2, "usage: kill pid...\n");
     exit();
   }
-  for(i=1; i<argc; i++)
-    kill(atoi(argv[i]));
+  for(i=1; i<argc; i++){
+    if(atoi(argv[i])==1){
+        printf(2,"No mates eso bro, no seas loko \n");
+    }else{
+        kill(atoi(argv[i]));
+    }
+    
+  }
   exit();
 }
