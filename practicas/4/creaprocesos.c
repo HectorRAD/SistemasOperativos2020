ff0b87e8b623823516356d7981983ed4b667f952
HectorRAD <a01636166@itesm.mx>
2020-09-30 00:31:52 +0000

    Practica 4

---
+++ practicas/4/creaprocesos.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+
+int status;
+
+void signalHandler(int signum){
+    printf("Limpiando hijo\n");
+    wait(&status);
+    printf("termina hijo con estatus %d\n",status);
+}
+
+
+int main(int argc, char **argv){
+    signal(17,signalHandler);
+    unsigned pid = fork();
+    if(pid==0){
+        sleep(5);
+        printf("Soy el proceso hijo\n");
+
+    } else {
+
+        sleep(2);
+        printf("Soy el proceso padre y mi hijo es %u\n",pid);
+        while(1){
+            sleep(2);
+            printf("trabajando\n");
+        }
+
+    }
+    printf("Hola mundo \n");
+
+
+    return 0;
+} 
